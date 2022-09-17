1d0f748e6fcfe0d222547dd082ef6cb5f5b2e384
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-17 17:49:21 -0500

    figure out HTML classes/define some variables

---
+++ Develop/script.js
@@ -0,0 +1,75 @@
+//variables
+//jumbotron
+var day = moment().format('dddd,MMMM Do');
+dayElement.text(day);
+
+//use JQuery to grab current day id and container div from HTML
+var dayElement = $('#currentDay');
+var timeBlockDiv = $('.container');
+var currentTime = moment().hour();
+
+//Array of hours for current day
+var Hours = [
+
+    moment().hour(24).format('hA'),
+    moment().hour(1).format('hA'),
+    moment().hour(2).format('hA'),
+    moment().hour(3).format('hA'),
+    moment().hour(4).format('hA'),
+    moment().hour(5).format('hA'),
+    moment().hour(6).format('hA'),
+    moment().hour(7).format('hA'),
+    moment().hour(8).format('hA'),
+    moment().hour(9).format('hA'),
+    moment().hour(10).format('hA'),
+    moment().hour(11).format('hA'),
+    moment().hour(12).format('hA'),
+    moment().hour(13).format('hA'),
+    moment().hour(14).format('hA'),
+    moment().hour(15).format('hA'),
+    moment().hour(16).format('hA'),
+    moment().hour(17).format('hA'),
+    moment().hour(18).format('hA'),
+    moment().hour(19).format('hA'),
+    moment().hour(20).format('hA'),
+    moment().hour(21).format('hA'),
+    moment().hour(22).format('hA'),
+    moment().hour(23).format('hA'),
+];
+
+//column for div that holds timeblock hour
+var timeBlock = $('col-1 hour')
+
+//div that holds the task
+var task = $('.description')
+
+//add timeblocks
+for (var i= 0; i<Hours.length;i++){
+    //adds rows to div
+    var row =$('<div>')
+    .addClass ('row time')
+    .attr ({
+        id: 'row-' + (i+24)
+    })
+
+    var timeBlock = $('<div>')
+    .addClass('col-1 hour')
+    .text(Hours[i])
+    .attr ({
+        id: i+ 24
+    })
+
+    var taskArea = $('<div>')
+    .addClass('col-10')
+    .attr ({
+        id: 'time-block-'+(i+24)
+    })
+    
+    var taskInput = $('<p>')
+    .addClass  ('description')
+    .text (' ')
+    .attr({
+        id: 'Hour-'(i+9)
+    });
+
+}
